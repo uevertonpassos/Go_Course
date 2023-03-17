c017ca822370201a85e3aa6f3ce78b2671d200de
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-17 15:51:07 -0300

    Atualizações
    contexto de system pause no go

--- pacotes/main.go
+++ pacotes/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"modulo/auxiliar"
+	"os"
 )
 
 func main() {
 	fmt.Println("Tentando buid")
 	auxiliar.Escrevendo()
 	auxiliar.Fixando()
+	bufio.NewReader(os.Stdin).ReadBytes('\n') //pausando o código compilado
 
 }
