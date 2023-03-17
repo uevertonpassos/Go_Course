335224cb94b1bed68e59eb9db45046b2200e9adc
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-17 18:29:06 -0300

    Create tentandoformatar.go
    aprendendo a usar format

---
+++ formats/tentandoformatar.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	x := 30
+	y := 20
+
+	fmt.Printf("%v + %v = %v e é um %T", x, y, x+y, x+y)
+	// ISTO É UM PRINT FORMATADO, FAVOR LEMBRAR DE USAR O PRINTF AO INVES DO
+	//PRINTLN
+
+}
