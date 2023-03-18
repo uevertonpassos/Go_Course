55292337631088eb79ba3b072df22f726df28e0e
Ueverton Passos <uevertonpassos96@gmail.com>
2023-03-18 01:56:41 -0300

    Treinando imports e modulos

---
+++ formats/importando/segundapagina.go
@@ -0,0 +1,5 @@
+package importando
+
+func Numeros(n1, n2 int8) int8 {
+	return n1 + n2
+}
--- formats/tentandoformatar.go
+++ formats/tentandoformatar.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"modulo/importando"
 )
 
 func main() {
@@ -9,6 +10,7 @@ func main() {
 	x := 30
 	y := 20
 
+	fmt.Println(importando.Numeros(3, 4))
 	fmt.Printf("%v + %v = %v e é um %T", x, y, x+y, x+y)
 	// ISTO É UM PRINT FORMATADO, FAVOR LEMBRAR DE USAR O PRINTF AO INVES DO
 	//PRINTLN
